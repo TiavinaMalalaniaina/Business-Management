a023c4bb6a04e776ac79330cc0aca0ee2e7bee1c
TiavinaMalalaniaina <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2023-10-28 09:22:29 +0300

    Initializing project

---
+++ src/main/java/mg/business_management/BusinessManagementApplication.java
@@ -0,0 +1,13 @@
+package mg.business_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagementApplication.class, args);
+	}
+
+}
